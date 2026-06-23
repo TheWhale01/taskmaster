17843710eef4c762c15358689e446aa585712a30
TheWhale01 <ard.rasp01@gmail.com>
2026-06-23 23:26:34 +0200

    first commit

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "taskmaster";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    uv2nix.url = "github:pyproject-nix/uv2nix";
+    pyproject-nix.url = "github:pyproject-nix/pyproject.nix";
+    build-systems.url = "github:pyproject-nix/build-system-pkgs";
+  };
+  outputs = { self, nixpkgs, uv2nix, pyproject-nix, build-systems }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+    workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+    overlay = workspace.mkPyprojectOverlay { sourcePreference = "wheel"; };
+    python = pkgs.python314;
+    pythonSet = (pkgs.callPackage pyproject-nix.build.packages { inherit python; })
+      .overrideScope (pkgs.lib.composeManyExtensions [
+        build-systems.overlays.default
+        overlay
+      ]);
+    venv = pythonSet.mkVirtualEnv "taskmaster-env" workspace.deps.default;
+  in
+  {
+    packages.${system}.default = venv;
+    apps.${system}.default = {
+      type = "app";
+      program = "${venv}/bin/taskmaster";
+    };
+    devShells.${system}.default = pkgs.mkShell {
+      packages = [
+        pkgs.uv
+        venv
+      ];
+    };
+  };
+}
---
+++ readme.md
@@ -0,0 +1,20 @@
+# Taskmaster
+
+This project is about job control. It's made in Python.
+
+## How to setup 
+
+
+### Nix:
+
+```nix
+nix run .
+```
+
+### Other distributions
+
+> __*NOTE:*__ Please ensure that Python >=3.14 is installed
+
+```bash
+python3 src/main.py
+```
