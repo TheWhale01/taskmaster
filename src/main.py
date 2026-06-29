530019c62418e900ac709bbc2de620260ae720d3
TheWhale01 <ard.rasp01@gmail.com>
2026-06-29 18:01:36 +0200

    passing arguments to program

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
+import argparse
+
 def main():
-    print("Hello, World!")
+    parser = argparse.ArgumentParser()
+    parser .add_argument("config")
+    args = parser.parse_args()
+    print(args.config)
 
 if __name__ == '__main__':
     main()
