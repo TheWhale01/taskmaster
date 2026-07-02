bf93f73a7b535d0ef46de3df942898d8d4902a0f
TheWhale01 <ard.rasp01@gmail.com>
2026-07-02 22:00:47 +0200

    switch to queue architecture + handling exit codes

--- flake.nix
+++ flake.nix
@@ -3,17 +3,42 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    uv2nix.url = "github:pyproject-nix/uv2nix";
+    pyproject-nix.url = "github:pyproject-nix/pyproject.nix";
+    build-systems.url = "github:pyproject-nix/build-system-pkgs";
   };
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, uv2nix, pyproject-nix, build-systems }:
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
+    workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+    overlay = workspace.mkPyprojectOverlay { sourcePreference = "wheel"; };
+    python = pkgs.python314;
+    pythonSet = (pkgs.callPackage pyproject-nix.build.packages { inherit python; })
+      .overrideScope (pkgs.lib.composeManyExtensions [
+        build-systems.overlays.default
+        overlay
+      ]);
+    venv = pythonSet.mkVirtualEnv "taskmaster-env" workspace.deps.default;
   in
   {
+    packages.${system}.default = venv;
+    apps.${system} = {
+      server = {
+        type = "app";
+        program = "${venv}/bin/server";
+      };
+      client = {
+        type = "app";
+        program = "${venv}/bin/client";
+      };
+    };
     devShells.${system}.default = pkgs.mkShell {
       packages = [
+        venv
         pkgs.uv
         pkgs.nginx
+        pkgs.python3
       ];
     };
   };
