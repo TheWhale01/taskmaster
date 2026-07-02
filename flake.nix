340a6ae4dc69c79f2952cfa4db917616d382c65e
TheWhale01 <ard.rasp01@gmail.com>
2026-07-02 20:33:54 +0200

    removed nix build system

--- flake.nix
+++ flake.nix
@@ -3,31 +3,17 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    uv2nix.url = "github:pyproject-nix/uv2nix";
-    pyproject-nix.url = "github:pyproject-nix/pyproject.nix";
-    build-systems.url = "github:pyproject-nix/build-system-pkgs";
   };
-  outputs = { self, nixpkgs, uv2nix, pyproject-nix, build-systems }:
+  outputs = { self, nixpkgs, ... }:
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
-    workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
-    overlay = workspace.mkPyprojectOverlay { sourcePreference = "wheel"; };
-    python = pkgs.python314;
-    pythonSet = (pkgs.callPackage pyproject-nix.build.packages { inherit python; })
-      .overrideScope (pkgs.lib.composeManyExtensions [
-        build-systems.overlays.default
-        overlay
-      ]);
-    venv = pythonSet.mkVirtualEnv "taskmaster-env" workspace.deps.default;
   in
   {
-    packages.${system}.default = venv;
     devShells.${system}.default = pkgs.mkShell {
       packages = [
         pkgs.uv
         pkgs.nginx
-        venv
       ];
     };
   };
